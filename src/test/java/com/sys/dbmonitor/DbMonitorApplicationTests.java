9d94201a9ded2efab42c5a1edb9b35bf54d570a3
baejiwon <qowhxk@naver.com>
2025-10-05 19:42:27 +0900

    Init Commit

---
+++ src/test/java/com/sys/dbmonitor/DbMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sys.dbmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
