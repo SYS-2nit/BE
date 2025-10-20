4f58bfebef6c3175ded8333e03e2ed0f3ccac277
JIWON <82360230+qowl880@users.noreply.github.com>
2025-10-20 16:00:34 +0900

    Feature/unux 51 base config (#4)
    * feat - DB Lock 동시성 처리 기능 구현
    
    * feat - Security 구현전 Header UserId Intercepter 구현
    
    * feat - Security 구현전 Header UserId Intercepter 구현
    
    * feat - Coupon 관련 CustomException 구현
    
    * feat - 더미데이터 쿼리문
    
    * feat - JMeter 대신 자체 테스트 구현
    
    * feat - JMeter 대신 자체 테스트 구현
    
    * feat - Redis & Kafka 기능 구현
    
    * feat - JMeterTest 구현
    
    * feat - Redis & Kafka 임시 주석처리
    
    * feat - Issue & PR Template
    
    * feat - DockerFile Add
    
    * feat - deploy name change
    
    * feat - deploy docker build Add
    
    * feat - GCP 연동
    
    * feat - GCP 연동
    
    * feat - Docker / Docker Compose 연동
    
    * feat - Docker / Docker Compose 연동
    
    * feat - MyBatisConfig 추가
    
    * feat - swagger 추가
    
    * feat - docker-compose 추가
    
    * feat - dev PR Image Push 분리

---
+++ src/main/java/com/sys/dbmonitor/global/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.sys.dbmonitor.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CCDB API 문서")
+                        .description("\uD83C\uDF31 Oracle DataBase Monitoring System")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("CCDB")
+                                .url("https://github.com/SYS-2nit"))
+                )
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("개발 서버")
+                ));
+    }
+
+}
+
+
