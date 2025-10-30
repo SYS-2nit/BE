799b69c7f3081068857d51da9f3155746ae2df24
baejiwon <qowhxk@naver.com>
2025-10-30 15:31:57 +0900

    feat - JPA 추가

--- src/main/java/com/sys/dbmonitor/DbMonitorApplication.java
+++ src/main/java/com/sys/dbmonitor/DbMonitorApplication.java
@@ -1,9 +1,17 @@
 package com.sys.dbmonitor;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.sys.dbmonitor")
+@MapperScan(basePackages = "com.sys.dbmonitor", annotationClass = org.apache.ibatis.annotations.Mapper.class)
+@EntityScan(basePackages = "com.sys.dbmonitor")
+@EnableJpaAuditing
 public class DbMonitorApplication {
 
     public static void main(String[] args) {
