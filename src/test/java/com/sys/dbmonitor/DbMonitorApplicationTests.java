d170a9f9e61bbf93fce789edeac7469d2bb8082e
baejiwon <qowhxk@naver.com>
2025-10-31 14:16:06 +0900

    feat - Test DB Connection 오류 해결

--- src/test/java/com/sys/dbmonitor/DbMonitorApplicationTests.java
+++ src/test/java/com/sys/dbmonitor/DbMonitorApplicationTests.java
@@ -2,12 +2,15 @@ package com.sys.dbmonitor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 테스트 프로파일 사용 (없으면 기본 설정 사용)
 class DbMonitorApplicationTests {
 
     @Test
     void contextLoads() {
+        // Spring Context가 정상적으로 로드되는지 확인
     }
 
 }
