aeac4aae0b1e654a90a20d920bb3880f7f161745
baejiwon <qowhxk@naver.com>
2025-11-04 10:59:27 +0900

    feat - Mybatis -> Jpa 구조 변경

--- src/main/java/com/sys/dbmonitor/DbMonitorApplication.java
+++ src/main/java/com/sys/dbmonitor/DbMonitorApplication.java
@@ -1,16 +1,11 @@
 package com.sys.dbmonitor;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@MapperScan(
-        basePackages = "com.sys.dbmonitor.domains.*.dao",
-        annotationClass = org.apache.ibatis.annotations.Mapper.class
-)
 @EntityScan(basePackages = "com.sys.dbmonitor")
 @EnableJpaAuditing  // JPA Auditing 기능 활성화 (@CreatedDate, @LastModifiedDate 등 사용)
 public class DbMonitorApplication {
