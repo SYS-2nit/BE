b7e9c385159f299477ef77c40fc13eacb25236dc
baejiwon <qowhxk@naver.com>
2025-11-09 22:43:56 +0900

    feat - 그래프 CRUD

---
+++ src/main/java/com/sys/dbmonitor/domains/graph/dto/response/GraphResponse.java
@@ -0,0 +1,34 @@
+package com.sys.dbmonitor.domains.graph.dto.response;
+
+import com.sys.dbmonitor.domains.graph.domain.Graph;
+import com.sys.dbmonitor.domains.graph.domain.GraphCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "그래프 응답")
+public record GraphResponse(
+        @Schema(description = "그래프 ID")
+        Long id,
+
+        @Schema(description = "그래프 이름")
+        String name,
+
+        @Schema(description = "그래프 카테고리")
+        GraphCategory category,
+
+        @Schema(description = "그래프 정보")
+        String info,
+
+        @Schema(description = "그래프 타입")
+        Integer type
+) {
+    public static GraphResponse from(Graph graph) {
+        return new GraphResponse(
+                graph.getId(),
+                graph.getName(),
+                graph.getCategory(),
+                graph.getInfo(),
+                graph.getType()
+        );
+    }
+}
+
